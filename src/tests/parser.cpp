947c0567dcf73ece475135c4e1dc9b79daa82c06
Tony Wasserka <NeoBrainX@gmail.com>
2015-06-30 21:38:43 +0200

    Assembler: Add lots of new tests

--- src/tests/parser.cpp
+++ src/tests/parser.cpp
@@ -25,9 +25,6 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <vector>
-#include <ostream>
-
 #include "nihstro/parser_assembly.h"
 #include "nihstro/parser_assembly_private.h"
 
@@ -37,9 +34,117 @@
 #define BOOST_TEST_MODULE Parser
 #include <boost/test/unit_test.hpp>
 
+// Implement some ostream<< operators for BOOST_CHECK*
+namespace std {
+
+template<typename T>
+std::ostream& operator << (std::ostream& os, const std::vector<T>& vec) {
+    auto it = vec.begin();
+    os << "{";
+    if (!vec.empty())
+        os << " " << *it;
+    while (it != vec.end() && ++it != vec.end()) {
+        os << ", " << *it;
+    }
+    os << " }";
+    return os;
+}
+
+std::ostream& operator << (std::ostream& os, const nihstro::Expression& expr) {
+    if (expr.signed_identifier.sign)
+        os << *expr.signed_identifier.sign;
+    os << expr.signed_identifier.identifier;
+    if (expr.index)
+        os << "[" << *expr.index << "]";
+    for (auto& mask : expr.swizzle_masks)
+        os << "." << mask;
+    return os;
+}
+
+std::ostream& operator << (std::ostream& os, const nihstro::IndexExpression& expr) {
+    for (size_t i = 0; i < expr.GetCount(); ++i) {
+        if (i != 0)
+            os << ", ";
+
+        if (expr.IsRawIndex(i)) {
+            os << expr.GetRawIndex(i);
+        } else if (expr.IsAddressRegisterIdentifier(i)) {
+            os << expr.GetAddressRegisterIdentifier(i);
+        } else {
+            os << "?";
+        }
+    }
+    return os;
+}
+
+std::ostream& operator << (std::ostream& os, const nihstro::IntegerWithSign& num) {
+    os << num.GetValue();
+    return os;
+}
+
+std::ostream& operator << (std::ostream& os, const nihstro::Instruction::FlowControlType::Op& op) {
+    if (op == Instruction::FlowControlType::And)
+        os << "&&";
+    else if (op == Instruction::FlowControlType::Or)
+        os << "||";
+    else
+        os << "??";
+
+    return os;
+}
+
+std::ostream& operator << (std::ostream& os, const nihstro::ConditionInput& inp) {
+    if (inp.invert)
+        os << "!";
+
+    os << inp.identifier;
+    if (inp.swizzler_mask)
+        os << "." << *inp.swizzler_mask;
+
+    return os;
+}
+
+std::ostream& operator << (std::ostream& os, const nihstro::Condition& cond) {
+    os << "{ " << cond.input1;
+
+    if (cond.op != Instruction::FlowControlType::JustX)
+        os << " " << cond.op << " " << cond.input2;
+
+    os << " }";
+    return os;
+}
+
+} // namespace std
+
+// Utility comparison operators
+namespace nihstro {
+bool operator == (const IntegerWithSign& a, const IntegerWithSign& b) {
+    return a.sign == b.sign && a.value == b.value;
+}
+
+bool operator == (const Expression& a, const Expression& b) {
+    bool ret = true;
+    ret &= a.signed_identifier.sign == b.signed_identifier.sign;
+    ret &= a.signed_identifier.identifier == b.signed_identifier.identifier;
+    ret &= a.index == b.index;
+    ret &= a.swizzle_masks == b.swizzle_masks;
+    return ret;
+}
+
+bool operator == (const ConditionInput& a, const ConditionInput& b) {
+    return a.invert == b.invert && a.identifier == b.identifier && a.swizzler_mask == b.swizzler_mask;
+}
+
+bool operator == (const Condition& a, const Condition& b) {
+    return a.input1 == b.input1 && a.input2 == b.input2 && a.op == b.op;
+}
+
+} // namespace nihstro
+
 // Utility function to parse the given input. Upon match, returns the parsed contents, and returns no value upon failure.
 template<typename Attr, typename Parser, typename Skipper>
 static boost::optional<Attr> parse(const std::string& input, const Parser& parser, const Skipper& skipper) {
+    BOOST_TEST_MESSAGE(("Parsing \"" + input + "\"").c_str());
     Attr attr;
     std::string inp(input);
     if (boost::spirit::qi::phrase_parse(inp.begin(), inp.end(), parser, skipper, attr))
@@ -52,23 +157,7 @@ static boost::optional<Attr> parse(const std::string& input, const Parser& parse
 template<typename T>
 static void CheckVector(const std::vector<T>& vec, const std::vector<T>& exp) {
     // TODO: Could just check directly for equality, but need an ostream<< operator for that.
-    BOOST_CHECK_EQUAL(vec.size(), exp.size());
-    for (size_t index = 0; index < vec.size() && index < exp.size(); ++index)
-        BOOST_CHECK_EQUAL(vec[index], exp[index]);
-}
-
-// Utility function to check that parsing of the first declaration statement was successfull and that the result matches the second declaration statements.
-static void CheckDeclaration(const boost::optional<StatementDeclaration>& declaration, const StatementDeclaration& exp) {
-    if (!declaration) {
-        BOOST_CHECK(false);
-    } else {
-        BOOST_CHECK_EQUAL(declaration->alias_name, exp.alias_name);
-        BOOST_CHECK_EQUAL(declaration->identifier_start, exp.identifier_start);
-        BOOST_CHECK_EQUAL(declaration->identifier_end, exp.identifier_end);
-        BOOST_CHECK_EQUAL(declaration->swizzle_mask, exp.swizzle_mask);
-        CheckVector(declaration->extra.constant_value, exp.extra.constant_value);
-        BOOST_CHECK_EQUAL(declaration->extra.output_semantic, exp.extra.output_semantic);
-    }
+    BOOST_CHECK_EQUAL(vec, exp);
 }
 
 // Utility function to convert a compile-time character to the corresponding swizzle mask component
@@ -99,6 +188,20 @@ static InputSwizzlerMask MakeInputSwizzlerMask() {
     return { 4, { MakeSwizzlerMaskComponent<a>(), MakeSwizzlerMaskComponent<b>(), MakeSwizzlerMaskComponent<c>(), MakeSwizzlerMaskComponent<d>() } };
 }
 
+// Utility function to check that parsing of the first declaration statement was successfull and that the result matches the second declaration statements.
+static void CheckDeclaration(const boost::optional<StatementDeclaration>& declaration, const StatementDeclaration& exp) {
+    if (!declaration) {
+        BOOST_CHECK(false);
+    } else {
+        BOOST_CHECK_EQUAL(declaration->alias_name, exp.alias_name);
+        BOOST_CHECK_EQUAL(declaration->identifier_start, exp.identifier_start);
+        BOOST_CHECK_EQUAL(declaration->identifier_end, exp.identifier_end);
+        BOOST_CHECK_EQUAL(declaration->swizzle_mask, exp.swizzle_mask);
+        CheckVector(declaration->extra.constant_value, exp.extra.constant_value);
+        BOOST_CHECK_EQUAL(declaration->extra.output_semantic, exp.extra.output_semantic);
+    }
+}
+
 BOOST_AUTO_TEST_CASE(declaration) {
     ParserContext context;
     DeclarationParser<std::string::iterator> declaration_parser(context);
@@ -123,6 +226,12 @@ BOOST_AUTO_TEST_CASE(declaration) {
     }
 
     {
+    // Output alias without output semantic
+    BOOST_CHECK_THROW(parse<StatementDeclaration>(".alias my_alias o5.xyz", declaration_parser, skipper),
+                      std::runtime_error);
+    }
+
+    {
     // Constant alias
     auto declaration = parse<StatementDeclaration>(".alias my_alias c5.xy as (1.0, -2.4)", declaration_parser, skipper);
     CheckDeclaration(declaration, { "my_alias", "c5", {}, MakeInputSwizzlerMask<'x', 'y'>(), { { 1.0, -2.4 } } });
@@ -138,5 +247,146 @@ BOOST_AUTO_TEST_CASE(label) {
     auto label = parse<StatementLabel>("my_label:", label_parser, skipper);
     BOOST_CHECK(label);
     BOOST_CHECK_EQUAL(*label, "my_label");
+    }
+}
+
+static void CheckParsedArithmeticInstruction(const boost::optional<FloatOpInstruction>& result, const FloatOpInstruction& exp) {
+    if (!result) {
+        BOOST_CHECK(false);
+    } else {
+        BOOST_CHECK_EQUAL(result->opcode, exp.opcode);
+        CheckVector(result->expressions, exp.expressions);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(arithmetic) {
+    ParserContext context;
+
+    FloatOpParser<std::string::iterator> arithmetic_parser(context);
+    AssemblySkipper<std::string::iterator> skipper;
+
+    {
+    // one-argument instruction
+    auto result = parse<FloatOpInstruction>("mova r2.xy", arithmetic_parser, skipper);
+    Expression r2_xy = { { {}, "r2" }, {}, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    CheckParsedArithmeticInstruction(result, { nihstro::OpCode::Id::MOVA, { r2_xy } });
+    }
+
+    {
+    // two-argument instruction
+    auto result = parse<FloatOpInstruction>("mov o0.wz, r2.xy", arithmetic_parser, skipper);
+    Expression o0_wz = { { {}, "o0" }, {}, { MakeInputSwizzlerMask<'w', 'z'>() } };
+    Expression r2_xy = { { {}, "r2" }, {}, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    CheckParsedArithmeticInstruction(result, { nihstro::OpCode::Id::MOV, { o0_wz, r2_xy } });
+    }
+
+    {
+    // two-argument instruction with trivial register index
+    auto result = parse<FloatOpInstruction>("mov o0.wz, r2[5].xy", arithmetic_parser, skipper);
+    IndexExpression index_expr;
+    index_expr.emplace_back(IntegerWithSign{ +1, 5 });
+    Expression o0_wz = { { {}, "o0" }, {}, { MakeInputSwizzlerMask<'w', 'z'>() } };
+    Expression r2_xy = { { {}, "r2" }, index_expr, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    CheckParsedArithmeticInstruction(result, { nihstro::OpCode::Id::MOV, { o0_wz, r2_xy } });
+    }
+
+    {
+    // two-argument instruction with nontrivial register index
+    auto result = parse<FloatOpInstruction>("mov o0.wz, r2[5+a1-4].xy", arithmetic_parser, skipper);
+    IndexExpression index_expr;
+    index_expr.emplace_back(IntegerWithSign{ +1, 5 });
+    index_expr.emplace_back("a1");
+    index_expr.emplace_back(IntegerWithSign{ -1, 4 });
+    Expression o0_wz = { { {}, "o0" }, {}, { MakeInputSwizzlerMask<'w', 'z'>() } };
+    Expression r2_xy = { { {}, "r2" }, index_expr, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    CheckParsedArithmeticInstruction(result, { nihstro::OpCode::Id::MOV, { o0_wz, r2_xy } });
+    }
+
+    {
+    // three-argument instruction
+    auto result = parse<FloatOpInstruction>("add o0.xy, r2.xy, r3.xy", arithmetic_parser, skipper);
+    Expression o0_xy = { { {}, "o0" }, {}, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    Expression r2_xy = { { {}, "r2" }, {}, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    Expression r3_xy = { { {}, "r3" }, {}, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    CheckParsedArithmeticInstruction(result, { nihstro::OpCode::Id::ADD, { o0_xy, r2_xy, r3_xy } });
+    }
+
+    {
+    // four-argument instruction
+    auto result = parse<FloatOpInstruction>("mad o0.xy, r2.xy, r3.xy, v4.xy", arithmetic_parser, skipper);
+    Expression o0_xy = { { {}, "o0" }, {}, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    Expression r2_xy = { { {}, "r2" }, {}, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    Expression r3_xy = { { {}, "r3" }, {}, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    Expression v4_xy = { { {}, "v4" }, {}, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    CheckParsedArithmeticInstruction(result, { nihstro::OpCode::Id::MAD, { o0_xy, r2_xy, r3_xy, v4_xy } });
+    }
+}
+
+static void CheckParsedFlowControlInstruction(const boost::optional<FlowControlInstruction>& result, const FlowControlInstruction& exp) {
+    if (!result) {
+        BOOST_CHECK(false);
+    } else {
+        BOOST_CHECK_EQUAL(result->opcode, exp.opcode);
+        BOOST_CHECK_EQUAL(result->target_label, exp.target_label);
+        BOOST_CHECK_EQUAL(result->return_label, exp.return_label);
+        BOOST_CHECK_EQUAL(result->condition, exp.condition);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(flowcontrol) {
+    ParserContext context;
+    FlowControlParser<std::string::iterator> parser(context);
+    AssemblySkipper<std::string::iterator> skipper;
+
+    {
+    // If-conditionals with two arguments
+    auto result = parse<FlowControlInstruction>("if cc.x && !cc.y", parser, skipper);
+    Condition cond = { { false, "cc", MakeInputSwizzlerMask<'x'>() }, Instruction::FlowControlType::And, { true, "cc", MakeInputSwizzlerMask<'y'>() } };
+    CheckParsedFlowControlInstruction(result, { nihstro::OpCode::Id::GEN_IF, "__dummy", {}, cond } );
+    }
+
+    {
+    // If-conditionals with one argument with one component
+    auto result = parse<FlowControlInstruction>("if cc.x", parser, skipper);
+    Condition cond = { { false, "cc", MakeInputSwizzlerMask<'x'>() }, Instruction::FlowControlType::JustX };
+    CheckParsedFlowControlInstruction(result, { nihstro::OpCode::Id::GEN_IF, "__dummy", {}, cond } );
+    }
+
+    {
+    // If-conditionals with one argument with two components
+    auto result = parse<FlowControlInstruction>("if !cc.xy", parser, skipper);
+    Condition cond = { { true, "cc", MakeInputSwizzlerMask<'x', 'y'>() }, Instruction::FlowControlType::JustX };
+    CheckParsedFlowControlInstruction(result, { nihstro::OpCode::Id::GEN_IF, "__dummy", {}, cond });
+    }
+
+    {
+    // Loop instruction
+    auto result = parse<FlowControlInstruction>("loop i3.y", parser, skipper);
+    Condition cond = { { false, "i3", MakeInputSwizzlerMask<'y'>() }, Instruction::FlowControlType::JustX };
+    CheckParsedFlowControlInstruction(result, { OpCode::Id::LOOP, "__dummy", {}, cond });
+    }
+}
+
+static void CheckParsedCompareInstruction(const boost::optional<CompareInstruction>& result, const CompareInstruction& exp) {
+    if (!result) {
+        BOOST_CHECK(false);
+    } else {
+        BOOST_CHECK_EQUAL(result->opcode, exp.opcode);
+        CheckVector(result->arguments, exp.arguments);
+        CheckVector(result->ops, exp.ops);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(compare) {
+    ParserContext context;
+    CompareParser<std::string::iterator> parser(context);
+    AssemblySkipper<std::string::iterator> skipper;
+
+    {
+    // Two separate comparisons
+    auto result = parse<CompareInstruction>("cmp r5.xy, r2.zx, ==, <=", parser, skipper);
+    Expression r5_xy = { { {}, "r5" }, {}, { MakeInputSwizzlerMask<'x', 'y'>() } };
+    Expression r2_zx = { { {}, "r2" }, {}, { MakeInputSwizzlerMask<'z', 'x'>() } };
+    CheckParsedCompareInstruction(result, { OpCode::Id::CMP, { r5_xy, r2_zx }, { Instruction::Common::CompareOpType::Equal, Instruction::Common::CompareOpType::LessEqual } });
     }
 }
