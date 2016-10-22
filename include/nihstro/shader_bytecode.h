66e175ce1e5a737650f1351a2df6ffb3ec4a490c
Davide Italiano <dccitaliano@gmail.com>
2016-10-22 15:15:36 -0700

    Fix the build with clang 3.8.
    Otherwise we fail with 'control reaches the end of a non-void
    function'.

--- include/nihstro/shader_bytecode.h
+++ include/nihstro/shader_bytecode.h
@@ -86,6 +86,7 @@ struct SourceRegister {
             return value - 0x10;
         else if (GetRegisterType() == RegisterType::FloatUniform)
             return value - 0x20;
+        throw "this should never be reached";
     }
 
     static const SourceRegister FromTypeAndIndex(RegisterType type, int index) {
