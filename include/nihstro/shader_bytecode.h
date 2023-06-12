d23d1b1dd82841018e4b93978598e8e40ca0310e
prprprpony <3254tg89@gmail.com>
2023-06-12 16:11:06 +0800

    Update shader_bytecode.h to fix return warning
    Fixed /shader_bytecode.h:89:5: warning: control reaches end of non-void function [-Wreturn-type]

--- include/nihstro/shader_bytecode.h
+++ include/nihstro/shader_bytecode.h
@@ -86,6 +86,10 @@ struct SourceRegister {
             return value - 0x10;
         else if (GetRegisterType() == RegisterType::FloatUniform)
             return value - 0x20;
+        else {
+            // TODO: Should throw an exception or something.
+        }
+        return -1;
     }
 
     static const SourceRegister FromTypeAndIndex(RegisterType type, int index) {
